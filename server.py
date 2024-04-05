f4ab20cfa51c83e6072c60bbbc6e534e8ac55625
Cralence <mahlerrrr76@gmail.com>
2024-04-05 09:51:48 +0800

    update some typings

--- server.py
+++ server.py
@@ -2,25 +2,29 @@ import asyncio
 import websockets
 from config import config
 import sys
-from typing import List, Dict
+from typing import List, Dict, Set, Optional, Any
 import numpy as np
 import time
+from websockets.legacy.server import WebSocketServerProtocol as Socket
 
 
 class ChatServer:
     def __init__(self, config):
-        self.rooms = {}  # Maps room names to sets of websockets.
-        self.audio_buffers = {}  # Maps room names to dict of user_id to audio chunks, Dict[room name, Dict[socket, bytes]
-        self.mixing_tasks = {}  # Maps room names to asyncio tasks
+        # Maps room names to sets of websockets.
+        self.rooms: Dict[str, Set[Socket]] = {}
+        # Maps room names to dict of user_id to audio chunks
+        self.audio_buffers: Dict[str, Dict[Socket, asyncio.Queue]] = {}
+        # Maps room names to an asyncio task that do audio mixing
+        self.mixing_tasks: Dict[str, Any] = {}
 
         self.max_buffer_size = config["max_buffer_size"]
-        self.buffer_duration = config["chunk_size"] / config["rate"] * self.max_buffer_size
+        self.amplification_factor = config["amplification_factor"]
 
-    async def handler(self, websocket):
+    async def handler(self, websocket: Socket):
         room_name = await websocket.recv()  # First message is the room name
 
         if room_name not in self.rooms:
-            self.rooms[room_name] = set()
+            self.rooms[room_name]: Set[Socket] = set()
             self.audio_buffers[room_name] = {}
             self.mixing_tasks[room_name] = asyncio.create_task(self.mix_and_broadcast(room_name))
 
@@ -44,17 +48,19 @@ class ChatServer:
             else:
                 print(f"Client disconnected from {room_name}. Total clients in room: {len(self.rooms[room_name])}")
 
-    async def mix_and_broadcast(self, room_name):
+    async def mix_and_broadcast(self, room_name: str):
         while True:
             try:
                 # wait until all the buffers in the room exceed the max buffer size
                 all_buffers_full = False
                 while not all_buffers_full:
                     await asyncio.sleep(0.01)  # sleep for a short duration to avoid busy waiting
-                    all_buffers_full = all(buffer.qsize() >= self.max_buffer_size for buffer in self.audio_buffers[room_name].values())
+                    all_buffers_full = all(
+                        buffer.qsize() >= self.max_buffer_size for buffer in self.audio_buffers[room_name].values()
+                    )
 
                 # load self.max_buffer_size of audio chunks from the buffers
-                audio_chunks = {}
+                audio_chunks: Dict[Socket, List[bytes]] = {}
                 for client, buffer in self.audio_buffers[room_name].items():
                     audio_chunks[client] = [await buffer.get() for _ in range(self.max_buffer_size)]
 
@@ -75,12 +81,12 @@ class ChatServer:
             except Exception as e:
                 print(f'error found: {e}', file=sys.stderr)
 
-    def mix_audio(self, audio_chunks) -> bytes:
+    def mix_audio(self, audio_chunks: Dict[Socket, List[bytes]]) -> Optional[bytes]:
         if len(audio_chunks) == 0:
             return None
 
         # Mix the audio chunks by calculating the mean of each chunk over all clients
-        audio_chunks: List[List[bytes]] = list(audio_chunks.values()) # List of List of bytes
+        audio_chunks: List[List[bytes]] = list(audio_chunks.values())
 
         # joint the bytes in the list
         audio_chunks: List[bytes] = [b''.join(chunks) for chunks in audio_chunks]
@@ -92,7 +98,7 @@ class ChatServer:
         # mix the data by summing them
         mixed_chunk = np.sum(arrays, axis=0)
         # amplify the mixed data
-        mixed_chunk = mixed_chunk * config["amplification_factor"]
+        mixed_chunk = mixed_chunk * self.amplification_factor
         # clip the mixed data
         mixed_chunk = np.clip(mixed_chunk, -32768, 32767)
         # convert the mixed data back to int16
