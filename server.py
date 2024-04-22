5d375574c85ace312a12fb2abd051233ddfa32aa
e3u9h <dfz2003@126.com>
2024-04-22 17:45:10 +0800

    fix a bug

--- server.py
+++ server.py
@@ -147,10 +147,8 @@ class ChatServer:
                     if socket != websocket:
                         await socket.send(b'V' + client_name.encode('utf-8') + message[5:])
         finally:
-            if websocket in self.rooms[room_name]:
+            if websocket in self.rooms2[room_name]:
                 self.rooms2[room_name].remove(websocket)
-            if websocket in self.audio_buffers[room_name]:
-                del self.audio_buffers[room_name][websocket]
             if len(self.rooms2[room_name]) == 0:
                 print(f"No clients left in room: {room_name}, but the room remains until explicitly deleted.")
             else:
